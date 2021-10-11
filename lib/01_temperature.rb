0922282cae44280575212c9e8ba1fd4fa6030424
Matthieu Croset <crosetmatthieu@gmail.com>
2021-10-11 23:28:07 +0200

    e

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,18 +1,7 @@
-
-
-
 def ftoc (value)
- 
-  (value.to_f - 32) * (5.0/9)
-
+  (value.to_f - 32 ) * 5 / 9 
 end
 
-#puts ftoc.to_i
-
-
-def ctof
- 
-  return f = (gets.chomp.to_f *(9.0/5)) +32
-
-end
-
+def ctof (value)
+  (value.to_f * 9 / 5) +32
+end 
